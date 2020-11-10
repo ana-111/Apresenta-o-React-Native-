c11b1913b3049c65fdc62d3d7603d60ce97f6764
ana-111 <anacarolinaamaralsilva2003@gmail.com>
2020-11-10 15:03:47 -0300

    React Native (começo)

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const entrar = () => {
+    alert('${email} - ${password}');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Email</Text>
+      <TextInput
+        value={email} 
+        onChangeText={(text) => setEmail(text)} 
+        style={{ height : 40, width : 200, backgroundColor : 'white', borderColor : 'gray', borderWidth : 1}}/>
       <StatusBar style="auto" />
+
+      <Text>Password</Text>
+      <TextInput
+        value={password} 
+        onChangeText={(text) => setPassword(text)} 
+        style={{ height : 40, width : 200, backgroundColor : 'white', borderColor : 'gray', borderWidth : 1}}/>
+      <StatusBar style="auto" />
+
+      {/*React 
+      <button onClick=(event => entrar(event)} */}
+      
+      <Button
+        onPress = {entrar}
+        title = "Entrar"
+        color = "#841584"
+        acessibilidadeLabel = "Entrar"
+      />
     </View>
   );
 }
